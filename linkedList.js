0fac03e29240b57888d5c8a797c2e58da3ed597d
Kanojah <kanoa.hi@gmail.com>
2017-03-20 16:22:50 -1000

    17 done chee

--- linkedList.js
+++ linkedList.js
@@ -1,8 +1,3 @@
-/**
- * @name  linkedListGenerator
- * @description  Main Module
- * @return {Object} an object exposing methods to be used to manipulate a linked list
- */
 function linkedListGenerator(){
 
   var head = null;
@@ -20,16 +15,47 @@ function linkedListGenerator(){
     },
 
     add: function (newNodeA) {
-      return {
-        value: newNodeA,
-        next: null
-      };
+      var newNode = {
+          value: newNodeA,
+          next: null
+        };
+
+      if (head === null) {
+        head = newNode;
+        tail = head;
+
+      } else {
+        tail.next = newNode;
+        tail = newNode;
+      }
+
+      return newNode;
     },
 
     remove: function () {},
 
-    get: function () {},
+    get: function (x) {
+      if (head === null) {
+        return null;
+      }
+      var current = head;
+      for (var i = 0; i < x; i++) {
+        if (current.next === null) {
+          return false;
+        }
+          current = current.next;
+      }
+      return current;
+    },
 
     insert: function () {}
   };
 }
+var list = linkedListGenerator();
+list.add('news.ycombinator.com');
+list.add('mozilla.org');
+list.add('eff.org');
+list.add('icann.org');
+console.log(list.get(0));
+console.log(list.get(1));
+console.log(list.get(2));
